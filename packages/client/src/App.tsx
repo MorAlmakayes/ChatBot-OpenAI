2a8b316b6e3f7b96b296c5520e01b644245b2fb7
moralmakayes <moralmakayes1@gmail.com>
2025-12-03 17:38:44 +0200

    Set up Husky 2

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react"
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react'
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState(" ");
+const [message, setMessage] = useState(' ');
 
   useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
+    fetch ('/api/hello')
+      .then(res => res.json())
+      .then (data => setMessage(data.message));
   }, []);
-
-  return (
+  
+  return(
     <>
-      <p className="font-bold"> {message} </p>
-      <Button>Click me</Button>
+    <p className= "font-bold"> {message} </p>
+    <Button>"Click me</Button>
     </>
   )
+
 }
 
-export default App;
+export default App
